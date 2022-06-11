7f1576fcf15eb28a13e70febc6d16af5b436b7e6
Ahmeddzz <ahmedzahran95@hotmail.com>
2022-06-11 15:15:20 +0200

    add b to hats

--- ecom/src/components/directory/directory.component.jsx
+++ ecom/src/components/directory/directory.component.jsx
@@ -4,7 +4,7 @@ const Directory = () => {
   const categories = [
     {
       id: 1,
-      title: "hats",
+      title: "hatsb",
       imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
